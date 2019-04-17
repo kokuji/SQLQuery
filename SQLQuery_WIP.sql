bc08ddd5fde95fdeb3f831bc8815cf97bf7688e2
kokuji <kelli.okuji@gmail.com>
2019-04-16 22:04:39 -0700

    Upload modified homework

--- SQLQuery_WIP.sql
+++ SQLQuery_WIP.sql
@@ -1,4 +1,5 @@
 use sakila;
+
 -- 1a
 select  first_name, last_name from actor;
 
@@ -19,6 +20,7 @@ select actor_id, first_name, last_name
 from actor
 where last_name like "%li%"
 order by last_name, first_name;
+
 -- 2d Using "In" clause
 select country_id, country
 from country
@@ -27,13 +29,16 @@ where country in ("Afghanistan", "Bangladesh", "China");
 -- 3a
 alter table actor
 add column description blob; 
+
 -- 3b
 alter table actor
 drop column description;
+
 -- 4a
 select last_name, count(last_name) as "Name count"
 from actor
 group by last_name;
+
 -- 4b
 select last_name, count(last_name) as "Name_count"
 from actor
@@ -41,17 +46,18 @@ group by last_name
 having "Name_count" > 1;
 
 -- 4c 
-
 select*
 from actor
 where last_name = "Williams";
 update actor
 set first_name = "Harpo"
 where actor_id = 172; 
+
 -- 4d 
 update actor
 set first_name = "Groucho"
 where actor_id = 172;
+
 -- 5a
 show create table address;
 
@@ -69,6 +75,7 @@ SELECT first_name, last_name, sum(amount) as "Total revenue per employee"
 FROM revenue
 WHERE payment_date LIKE "2005-08%"
 GROUP BY last_name;
+
 -- 6c
 CREATE VIEW actor_list AS
 SELECT film.film_id, film.title, film_actor.actor_id
@@ -77,6 +84,7 @@ INNER JOIN film ON film.film_id=film_actor.film_id;
 SELECT actor_list.film_id, actor_list.title, COUNT(actor_list.actor_id) AS "Total actors in film"
 FROM actor_list
 GROUP BY actor_list.film_id;
+
 -- 6d
 SELECT COUNT(inventory_id) AS "Hunchback Impossible copies"
 FROM inventory
@@ -95,7 +103,6 @@ from inventory
 JOIN film on inventory.film_id=film.film_id;
  
 -- 6e
-
 -- Create a view to join the two tables
 CREATE VIEW customer_pay as
 SELECT customer.first_name, customer.last_name, customer.customer_id, payment.amount
@@ -114,7 +121,6 @@ SELECT*
 FROM customer_total_pay;
 
 -- 7a
-
 SELECT title
 FROM film
 WHERE (title LIKE "K%" AND language_id)
@@ -137,6 +143,7 @@ FROM film
 WHERE title = "Alone Trip"
 )
 );
+
 -- 7c
 SELECT cust.first_name, cust.last_name, cust.email, ctry.country
 FROM customer cust
@@ -145,7 +152,6 @@ JOIN city cty ON a.city_id = cty.city_id
 JOIN country ctry ON cty.country_id = ctry.country_id
 WHERE country = "Canada";
 -- 7d
-
 SELECT f.title, c.name
 FROM category c
 JOIN film_category fc ON c.category_id = fc.category_id
@@ -175,7 +181,6 @@ LEFT JOIN city cty ON a.city_id = cty.city_id
 LEFT JOIN country ctry ON cty.country_id = ctry.country_id;
 
 -- 7h
-
 SELECT c.name, sum(p.amount) AS "Total gross revenue (by genre)"
 FROM payment p
 LEFT JOIN rental r ON p.customer_id = r.customer_id
@@ -199,10 +204,8 @@ ORDER BY "Total gross revenue (by genre)" DESC
 limit 5;
 
 -- 8b
-
 SELECT *
 FROM top_five_revenue;
 
 -- 8c
-
 DROP VIEW top_five_revenue;
